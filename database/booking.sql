a3b9259e358cd14b3b70cf1e75e222c515f3dcf3
[Students] Kirils Kozemjakins <01DPKKozemjakins@rvt.lv>
2023-02-21 11:19:48 +0200

    Add access column to database(it can be changed in user_data.php)

--- database/booking.sql
+++ database/booking.sql
@@ -19,23 +19,24 @@ USE `booking`;
 
 -- Dumping structure for table booking.customer
 CREATE TABLE IF NOT EXISTS `customer` (
-  `CustomerID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(50) DEFAULT NULL,
   `LastName` varchar(50) DEFAULT NULL,
   `Password` varchar(50) DEFAULT NULL,
   `Email` varchar(50) DEFAULT NULL,
+  `Access` int(11) DEFAULT '0',
   PRIMARY KEY (`CustomerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table booking.customer: ~6 rows (approximately)
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` (`CustomerID`, `FirstName`, `LastName`, `Password`, `Email`) VALUES
-	(1, 'Kirils', 'Kozemjakins', 'qwerty12345', 'qwerty@gmail.com'),
-	(2, 'Aleksandrs', 'Loceks', '12345789', 'rvt@gmail.com'),
-	(3, 'Ilja', 'Rimsa', '12345789', 'rimsa@gmail.com'),
-	(4, 'Ivan', 'Aleksejevichs', '123654789', 'ivan@gmail.com'),
-	(5, 'Ilona', 'Grebkova', 'pivoBIseychasj', 'ilona@gmail.com'),
-	(6, 'Deniss', 'Kozlovs', 'kok', 'denis@inbox.lv');
+INSERT INTO `customer` (`CustomerID`, `FirstName`, `LastName`, `Password`, `Email`, `Access`) VALUES
+	(1, 'Kirils', 'Kozemjakins', 'qwerty12345', 'qwerty@gmail.com', 1),
+	(2, 'Aleksandrs', 'Loceks', '12345789', 'rvt@gmail.com', 1),
+	(3, 'Ilja', 'Rimsa', '12345789', 'rimsa@gmail.com', 0),
+	(4, 'Ivan', 'Aleksejevichs', '123654789', 'ivan@gmail.com', 0),
+	(5, 'Ilona', 'Grebkova', 'pivoBIseychasj', 'ilona@gmail.com', 0),
+	(6, 'Maksims', 'Visockis', 'sosiska123', 'sosiska@gmil.com', 0);
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 
 -- Dumping structure for table booking.demo1
@@ -45,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `demo1` (
   PRIMARY KEY (`DemoID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
--- Dumping data for table booking.demo1: ~14 rows (approximately)
+-- Dumping data for table booking.demo1: ~16 rows (approximately)
 /*!40000 ALTER TABLE `demo1` DISABLE KEYS */;
 INSERT INTO `demo1` (`DemoID`, `test`) VALUES
 	(3, 'test'),
