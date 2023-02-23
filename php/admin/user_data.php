125e3ac15d96b071fde45072b5b2b32ceecbe4ad
kkozemjakins <kkozemjakins@gmail.com>
2023-02-23 23:39:14 +0200

    added design to the page 'user_data'

--- php/admin/user_data.php
+++ php/admin/user_data.php
@@ -5,11 +5,46 @@
 	<title>booking</title>
 	<link rel="stylesheet" href="..\..\css\admin\user_data.css">
     <script src="..\..\js\user_data.js" defer></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form action='..\..\FunctionsPHP\logout.php' method="post">
-        <button type="submit" name="logout">Выйти</button>
-    </form>
+
+<header>
+    <div class="container row">
+        <a href="MainAdmin.php" class="logo">Resting</a>
+        <nav class="nav">
+        <ul class="nav__list nav__list--primary">
+            <li class="nav__item"><a href="ActivitiesAdmin.php" class="nav__link">Activities</a></li>
+            <li class="nav__item"><a href="user_data.php" class="nav__link">User data</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
+        </ul>
+        <ul class="nav__list">
+            <li class="nav__item">
+              <a class="nav__link">
+
+                <form action='..\..\FunctionsPHP\logout.php' method="post">
+                  <button type="submit" name="logout">Exit</button>
+
+                </form>
+              </a>
+            </li>
+            <li>
+              <a class="nav__link">
+                <?php
+                
+                  if (isset($_SESSION['authorized']) && $_SESSION['authorized'] === true) {
+          
+                    echo 'Welcome, ' . $_SESSION['username'] . '!';
+                  }
+                ?>
+              </a>
+            </li>
+        </ul>
+        </nav>
+    </div>
+    </header>
     <!-- Display data from the users table -->
     <table class="styled-table">
         <tr>
