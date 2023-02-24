a2098bb8df0e8d53336afbbe1b76874fbfbeb6d1
kkozemjakins <kkozemjakins@gmail.com>
2023-02-24 12:15:36 +0200

    Now, along with the offer, the picture is also deleted

--- FunctionsPHP/delete_activities.php
+++ FunctionsPHP/delete_activities.php
@@ -9,7 +9,7 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = $conn->prepare("DELETE FROM offers WHERE OffersID = :OffersID");
+    $query = $conn->prepare("DELETE offers, images FROM offers LEFT JOIN images on offers.ImageID = images.ImageID WHERE OffersID = :OffersID");
     $query->bindParam(':OffersID', $_POST['OffersID']);
     $query->execute();
     header('Location: ..\php\admin\ActivitiesAdmin.php');
